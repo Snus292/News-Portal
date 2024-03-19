8f4d8526e8e9403617fc008fcda92b69adbb7960
DOOM097 <98549250+DOOM097@users.noreply.github.com>
2024-03-19 15:18:17 +0200

    Add files via upload
    NP-20 добавлена верстка веб приложения на основе PHP

---
+++ newsPortalV2/controller/Controller.php
@@ -0,0 +1,31 @@
+<?php
+
+class Controller {
+    public static function StartSite() {
+        $arr = News::getLast10News();
+        include_once 'view/start.php';
+    }
+
+    public static function AllCategory() {
+        $arr = Category::getAllCategory();
+        include_once 'view/category.php';
+    }
+
+    public static function AllNews(){
+        $arr = News::getAllNews();
+        include_once 'view/allnews.php';
+    }
+
+    public static function NewsByCatID($id) {
+        $arr = News::getNewsByCategoryID($id);
+        include_once 'view/catnews.php';
+    }
+    public static function NewsById($id)    {
+        $n = News::getNewsByID($id);
+        include_once 'view/readnews.php';
+    }
+
+    public static function error404() {
+        include_once 'view/arror404.php';
+    }
+}
