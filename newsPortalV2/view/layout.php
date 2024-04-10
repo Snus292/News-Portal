1757ff25dc23ee9f2c49862b4a9ed2c325739014
Snus292 <georgiigiblov@gmail.com>
2024-04-10 10:16:46 +0300

    переход на 4.6 версию бутсрапа , исправленна ошибка во view/layout.php

--- newsPortalV2/view/layout.php
+++ newsPortalV2/view/layout.php
@@ -2,11 +2,7 @@
 <html>
     <head>
         <title> NEWSPORTAL</title>
-            <link rel="stylesheet" href="
-            https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
-            integrity= "sha384-ggoyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
-            crossorigin="anonymous">
-            <link rel="stylesheet" type="text/css" href="style.css">
+            <link rel="stylesheet" href="../public/css/bootstrap.min.css">
             <link href="https://fonts.googleapis.com/css?family=Noto+Serif" rel="stylesheet"> 
         <meta charset="utf-8">
     </head>
@@ -15,9 +11,9 @@
         <ul class="topmenu">
             <li><a href="#">Kategooriad<i class="fa fa-angle-down"></i></a>
                 <ul class="submenu">
-                    <?php
-                            Controller: AllCategory();
-                    ?>
+                    
+                <?php Controller::AllCategory(); ?>
+                    
                 </ul>
             </li>
             <li><a href="iwww">Info</a></li>
@@ -48,4 +44,5 @@
     </section>
     <hr>
     <p style="display:block; text-align:center;">SPTV21 2024 a. &copy</p>
+    <script src="../public/js/bootstrap.min.js"></script>
 </body>
