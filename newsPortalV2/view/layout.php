0bb96bad9b191e017518ed89acf90c3b89eeae0e
Snus292 <georgiigiblov@gmail.com>
2024-04-16 12:41:43 +0300

    устранение ошибок с бутстапом, переход на временную ветку

--- newsPortalV2/view/layout.php
+++ newsPortalV2/view/layout.php
@@ -2,7 +2,9 @@
 <html>
     <head>
         <title> NEWSPORTAL</title>
-            <link rel="stylesheet" href="../public/css/bootstrap.min.css">
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
+            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+            <link rel="stylesheet" type="text/css" href="style.css">
             <link href="https://fonts.googleapis.com/css?family=Noto+Serif" rel="stylesheet"> 
         <meta charset="utf-8">
     </head>
@@ -10,10 +12,10 @@
     <nav class="one">
         <ul class="topmenu">
             <li><a href="#">Kategooriad<i class="fa fa-angle-down"></i></a>
-                <ul class="submenu">
-                    
-                <?php Controller::AllCategory(); ?>
-                    
+                <ul class="submenu">              
+
+                    <?php Controller::AllCategory(); ?>
+
                 </ul>
             </li>
             <li><a href="iwww">Info</a></li>
@@ -44,5 +46,4 @@
     </section>
     <hr>
     <p style="display:block; text-align:center;">SPTV21 2024 a. &copy</p>
-    <script src="../public/js/bootstrap.min.js"></script>
 </body>
